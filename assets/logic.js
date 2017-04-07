4cda34170d3acf2c4899521b932cef4de234400b
Angelo <aflorend@gmail.com>
2017-04-07 14:02:14 -0500

    Minor additions to logic and html

---
+++ assets/logic.js
@@ -0,0 +1,21 @@
+// $(document).ready(function() {
+	var choice1;
+	var choice2;
+
+	var confirm1 = false;
+	var confirm2 = false;
+
+	$('.rps1').on('click', function() {
+		choice1 = $(this).attr('data-choice');
+		console.log(choice1);
+		console.log("yup");
+
+		// $('.player1-div').toggleClass
+
+		// $('.rps1').off('click');
+	})
+
+	// if (confirm1 === true && confirm2 === true) {
+
+	// }
+// })
